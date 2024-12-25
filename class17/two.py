e0b8b0b0c3ae6ce098771b1733787be14b1598b0
sheikshammadh <sheikshammadh18@gmail.com>
2024-12-25 18:29:14 +0530

    sn

--- class17/two.py
+++ class17/two.py
@@ -1,9 +1,23 @@
-def login_status(func):
-    def inner(name,login_status):
-        if status==False:
+def login_required(func):
+    def inner(name,status):
+        if status== False:
             print("login required")
         else:
-            func(name,login_status)
+            func(name,status)
+
     return inner
-login_status(name,login_status)
-   
+
+def home_page(name,login_status):
+    print("home page")
+def about_page(name,login_status):
+    print("about page")
+@login_required
+def history_page(name,login_status):
+    print("history page")
+@login_required
+def profile_page(name,login_status):
+    print("profile page")   
+home_page("shyam",True)
+about_page("shyam",True)
+history_page("shyam",False)
+profile_page("shyam",False)
